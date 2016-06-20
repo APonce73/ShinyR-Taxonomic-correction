19a3e9e951b674b824a84184f16b8605d3aa4909
Alejandro Ponce Mendoza <aponce73pm@gmail.com>
2016-06-20 16:30:11 -0500

    Change from rinat to spocc package.

--- server.R
+++ server.R
@@ -1,4 +1,8 @@
 library(shiny)
+library(leaflet)
+library(RColorBrewer)
+
+
 
 # Define server logic for slider examples
 shinyServer(function(input, output, session) {
@@ -34,38 +38,62 @@ shinyServer(function(input, output, session) {
     #print(taxize1)  
       
     })
-  Termsinat<-reactive({
+  
+  #For the MAP
+  points<-reactive({
     input$update
-    
-    require(rinat)
-    library(ggmap)
-    library(ggplot2)
+    require(leaflet)
+    require(spocc)
+    #require(ggmap)
+    #require(ggplot2)
     require(taxize)
     VALOR<-input$text
     LL1<- tnrs(query = VALOR, source = "iPlant_TNRS")
     LL1a<-as.data.frame(t(LL1))
     VALOR1<-LL1$matchedname
+    rm(LL1)
     #VALOR<-levels(taxize1[4,1])[2]
-    Data1 <- get_inat_obs(query=VALOR1, quality="research")
     
-    #Data1
-    #Data1$scientific_name
-    #names(Data1)
-    #El primer renglon son los datos de Nestor
+    LL2 <- occ(VALOR1, from="inat", has_coords=T)
+    LL2 <- occ2df(LL2)
+    LL2 <- data.frame(LL2$longitude, LL2$latitude)
+    names(LL2)[1] <- c("longitude")
+    names(LL2)[2] <- c("latitude")
+    print(LL2)
+    })
     
-    map <- get_map(location = "Mexico", zoom = 5, source = "google")
-    p <- ggmap(map, fullpage = TRUE)
-    p
-    #Ahora adicionamos los datos
-    p+ geom_point(aes(x = longitude, y = latitude, colour = scientific_name), size=3,data = Data1)
+  
+  output$mymap <- renderLeaflet({
     
+    #pal3 <- pal2
+    TTT <- sample(brewer.pal(8,"Dark2"),1)
+    Goldberg <- points()
+    #Goldberg1 <-InterEstellar() 
+    leaflet(data = Goldberg) %>%
+      clearShapes() %>%
+      addTiles() %>%
+      setView(lng=-101.383331, lat=22.583332, zoom=6, options=list(reset=T))%>%
+      #addCircleMarkers(~longitude, ~latitude, radius=2, fillOpacity = 0.5, color=TTT[1])%>%
+      addCircleMarkers(Goldberg$longitude, Goldberg$latitude, weight=4, radius=7, stroke=F, fillOpacity = 0.8, color=TTT[1],clusterOptions = markerClusterOptions())%>%
+      #addMeasure(primaryLengthUnit = "kilometers", primaryAreaUnit = "hectares",activeColor = '#FF00FF')%>%
+      #addProviderTiles("OpenStreetMap.DE")
+      addProviderTiles("OpenStreetMap.HOT")
+    #addPolygons(stroke=F, smoothFactor=0, fillColor='#FF00FF', fillOpacity=0.7, data=points())
+    #mapOptions(zoomToLimits="first")
+    #addCircleMarkers(~longitude, ~latitude, popup =paste(sep = " ","Altitud=",as.factor(TableL$Altitud), "msnm","Altura del maiz",as.factor(TableL$Altura_de_planta),"cm"), radius=3, fillOpacity = 0.5, color=UUUH())
   })
+  
+  
 
   output$TableL<-renderTable({
     TermsTaxonstand()
   })
   output$TableL1<-renderTable({
     TermsTaxize()
+  })
+  output$Tabla33<-renderTable({
+    Goldberg <- points()
+    print(Goldberg)
   })
   output$map1<-renderPlot({
     Termsinat()
--- ui.R
+++ ui.R
@@ -1,11 +1,22 @@
+library(shiny)
+library(leaflet)
+
 shinyUI(fluidPage(
-  titlePanel("Correccion Taxon??mica"),
+  titlePanel("Taxonomic corrections"),
+  h6("We use two different packages in R to do almost the same analysis."),
+  h6("The functions in R use different algorithm to correct"),
+  h6("mispelling or sinonyms of flora."),
+  h3("Why Plant list?"),
+  #h6("Plant list is a database with the support of Missouri Botanical Garden and the Royal Botanical Gardens, Kew"),
+  h4("Important notes:"),
+  h6("if you decide to use this plataform, please do credits to each package in R!"),
+  
   
   fluidRow(
     
     column(3, 
            textInput("text", label = h3("Especie vegetal"), 
-                     value = "Helianthus annuus")),
+                     value = "Pinus cembroides")),
     column(3,
            h3("Actualizar"),
            #actionButton("update", label = "Action"),
@@ -14,15 +25,30 @@ shinyUI(fluidPage(
            br()
            ),
     column(8,
-           h4("Resumen Taxonstand"),
+          h4("Taxonstand results"),
       tableOutput("TableL"),
-          h6("Cayuela, L. Granzow-de la Cerda,I. Albuquerque FS, Golicher DJ (2012), taxonstand: An r package for species names stndardisation in vegetation database. Methods in Ecology and Evolution, 3: 1078-1083. doi: 10.1111/j.2041-210X2012.00232.X"),
-          h4("Resumen taxize"),
+          h5("Citation of Taxonstand package:"),
+          h6("Cayuela, L. Granzow-de la Cerda,I. Albuquerque FS, Golicher DJ (2012),"), 
+          h6("taxonstand: An r package for species names stndardisation in vegetation database." ),   
+          h6("Methods in Ecology and Evolution, 3: 1078-1083." ),   
+          h6("doi: 10.1111/j.2041-210X2012.00232.X"),
+          h4("taxize results"),
           tableOutput("TableL1"),
-          h6("Scott Chamberlain and Eduard Szocs (2013). taxize - taxonomic search and retrieval in R. F1000Research, 2:191. URL: http://f1000research.com/articles/2-191/v2. ")
+          h5("Citation of taxize package:"),
+          h6("Scott Chamberlain and Eduard Szocs (2013). taxize -"), 
+          h6("taxonomic search and retrieval in R. F1000Research, 2:191."),    
+          h6("URL: http://f1000research.com/articles/2-191/v2.")
+          #tableOutput("Tabla33")
+      
     ),
-    mainPanel(plotOutput("map1"))
-    
+    column(4,h5("All sample points (dots) come from iNaturalist database"),
+      column(12,leafletOutput("mymap",height=800))
+      #h4("Citation of rinat package:"),
+      #h6("Citation: : Vijay Barve and Edmund Hart (2014). rinat: Access iNaturalist data through APIs."), 
+      #h6("R package version 0.1.4."),    
+      #h6("http://CRAN.R-project.org/package=rinat")
+      )
+      
   )
   
 ))
