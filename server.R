200218ac14c92db7599c0626df6200746ee123bd
Alejandro Ponce Mendoza <aponce@conabio.gob.mx>
2017-03-11 18:14:28 -0600

    change the order
    Put before the table and later the graphics

--- server.R
+++ server.R
@@ -7,62 +7,68 @@ library(RColorBrewer)
 # Define server logic for slider examples
 shinyServer(function(input, output, session) {
   
-  TermsTaxonstand<-reactive({
+  TermsTaxonstand <- reactive({
    
    
     require(Taxonstand)
    
-    VALOR<-input$text
-    LL<- TPLck(VALOR)
+    VALOR <- input$text
+    #VALOR <- c("Acacia Angustissima")
+    LL <- TPLck(VALOR)
+    t(LL)
+    LLa <- LL[c(8,9,10,12,13,14)]
+    t(LLa)
+    #vec1 <- as.vector(names(LLa))
+    FF <- as.data.frame(t(LLa))
+    dim(FF)
+    names(FF)[1] <- c("Taxonstand")
+    Taxonstand1 <- FF
     
-    LLa<-LL[c(8,9,10,12,13,14)]
-    FF<-as.data.frame(t(LLa))
-    names(FF)[1]<-c("Taxonstand")
-    Taxonstand1<-FF
-    #print(Taxonstand1)
     
   })
     # Show the values using an HTML table
   
-  TermsTaxize<-reactive({
+  TermsTaxize <- reactive({
     input$update
     require(taxize)
     library(curl)
-    VALOR<-input$text
+    VALOR <- input$text
     #VALOR<-"gossipium hirsutum"
-    LL1<- tnrs(query = VALOR, source = "iPlant_TNRS")
+    LL1 <- tnrs(query = VALOR, source = "iPlant_TNRS")
     
-    LL1a<-as.data.frame(t(LL1))
-    names(LL1a)[1]<-c("taxize")
-    taxize1<-LL1a
-    #print(taxize1)  
+    LL1a <- as.data.frame(t(LL1))
+    names(LL1a)[1] <- c("taxize")
+    taxize1 <- LL1a
       
     })
   
   #For the MAP
-  points<-reactive({
+  points <- reactive({
     input$update
     require(leaflet)
     require(spocc)
     #require(ggmap)
     #require(ggplot2)
     require(taxize)
-    VALOR<-input$text
-    LL1<- tnrs(query = VALOR, source = "iPlant_TNRS")
-    LL1a<-as.data.frame(t(LL1))
-    VALOR1<-LL1$matchedname
+    VALOR <- input$text
+    LL1 <- tnrs(query = VALOR, source = "iPlant_TNRS")
+    LL1
+    LL1a <- as.data.frame(t(LL1))
+    VALOR1 <- LL1$matchedname
     rm(LL1)
     #VALOR<-levels(taxize1[4,1])[2]
     #L <- c()
     #L2 <- c(input$gbif)
     #FF <- c(L,L2)
     
-    LL2 <- occ(VALOR1, from="inat", has_coords=T)
+    LL2 <- occ(VALOR1, from = "gbif", has_coords = T)
+    #LL2
     LL2 <- occ2df(LL2)
+    str(LL2)
+    #LL2
     LL2 <- na.omit(data.frame(LL2$longitude, LL2$latitude))
-    names(LL2)[1] <- c("longitude")
-    names(LL2)[2] <- c("latitude")
-    
+    names(LL2)[1] <- c("lng")
+    names(LL2)[2] <- c("lat")
     
     #LL3 <- occ(VALOR1, from=input$gbif, has_coords=T)
     #LL3 <- occ2df(LL3)
@@ -70,7 +76,7 @@ shinyServer(function(input, output, session) {
     #names(LL3)[1] <- c("longitude")
     #names(LL3)[2] <- c("latitude")
     #LL4 <- rbind(LL2,LL3)
-    print(LL2)
+    #print(LL2)
     })
     
   
@@ -79,13 +85,15 @@ shinyServer(function(input, output, session) {
     #pal3 <- pal2
     TTT <- sample(brewer.pal(8,"Dark2"),1)
     Goldberg <- points()
-    #Goldberg1 <-InterEstellar() 
+    #Goldberg <- LL2
+    
+    #Goldberg1 <-InterEstellar()
     leaflet(data = Goldberg) %>%
       clearShapes() %>%
       addTiles() %>%
-      setView(lng=-101.383331, lat=22.583332, zoom=6, options=list(reset=T))%>%
+      setView(lng=-101.383331, lat=22.583332, zoom=5, options=list(reset=T))%>%
       #addCircleMarkers(~longitude, ~latitude, radius=2, fillOpacity = 0.5, color=TTT[1])%>%
-      addCircleMarkers(Goldberg$longitude, Goldberg$latitude, weight=4, radius=7, stroke=F, fillOpacity = 0.8, color=TTT[1],clusterOptions = markerClusterOptions())%>%
+      addCircleMarkers(Goldberg$lng, Goldberg$lat, weight=4, radius=7, stroke=F, fillOpacity = 0.8, color=TTT[1],clusterOptions = markerClusterOptions())%>%
       #addMeasure(primaryLengthUnit = "kilometers", primaryAreaUnit = "hectares",activeColor = '#FF00FF')%>%
       #addProviderTiles("OpenStreetMap.DE")
       addProviderTiles("OpenStreetMap.HOT")
@@ -95,19 +103,23 @@ shinyServer(function(input, output, session) {
   })
   
   
-
-  output$TableL<-renderTable({
-    TermsTaxonstand()
-  })
-  output$TableL1<-renderTable({
-    TermsTaxize()
-  })
-  output$Tabla33<-renderTable({
-    Goldberg <- points()
-    print(Goldberg)
-  })
-  output$map1<-renderPlot({
-    Termsinat()
+#  output$Ceratti2 <- renderPrint({
+#    summary(Ceratti1())
+#  })  
+  
+  
+  output$TableL <- renderTable(
+    print(TermsTaxonstand()), rownames = T)
+  
+  output$TableL1 <- renderTable(
+    print(TermsTaxize()), rownames = T
+  )
+#  output$Tabla33 <- renderTable({
+#    Goldberg <- points()
+#    print(Goldberg)
+#  })
+  output$map <- renderPlot({
+    mymap()
   })
 })
   
--- ui.R
+++ ui.R
@@ -2,55 +2,159 @@ library(shiny)
 library(shinydashboard)
 library(leaflet)
 
-shinyUI(bootstrapPage(
-  titlePanel("Taxonomic corrections"),
-  h6("We use two different packages in R to do almost the same analysis. The functions in R use different algorithm to correct"),
-  h6("mispelling or synonyms of flora."),
+
+################################
+
+#dashboardPage(
+#  dashboardHeader(),
+#  dashboardSidebar(),
+#  dashboardBody()
+#)
+
+
+
+shinyUI(navbarPage(
+  #title = "Mauricio Bellon paper",
+  title = "Taxonomic corrections",
+  h5("We use two different packages in R to do almost the same analysis. The functions in R use different algorithm to correct"),
+  h5("mispelling or synonyms of flora."),
   h3("Why Plant list?"),
-  h6("Plant list is a database with the support of Missouri Botanical Garden and the Royal Botanical Gardens, Kew"),
+  h5("Plant list is a database with the support of Missouri Botanical Garden and the Royal Botanical Gardens, Kew"),
   h4("Important notes:"),
-  h6("if you decide to use this plataform, please do credits to each package in R!"),
+  h5("if you decide to use this plataform, please do credits to each package in R!"),
   
   
-  fluidRow(
-    column(3, 
-           textInput("text", label = h3("Plant Species"), 
-                     value = "Pinus cembroides")),
-   
-    column(3,
-      h3("Actualizar"),
-      #actionButton("update", label = "Action"),
-      submitButton("Submit"),
-      br(),
-      br()
-    ),
-    column(10,h5("All sample points (dots) come from iNaturalist database"),
-      column(12,leafletOutput("mymap",height=800)),
-      h4("Citation of leaflet package:"),
-      h6("Citation: : Joe Cheng and Yihui Xie (2016). Leaflet: Create Interactive web Maps with the JavaScript 'leaflet' Library"), 
-      h6("R package version 1.0.1.900."),    
-      h6("http://rstudio.github.io/leaflet/")
-    ),
-    column(6,
-      h4("Taxonstand package result:"),
-      tableOutput("TableL"),
-          h5("Citation of Taxonstand package:"),
-          h6("Cayuela, L. Granzow-de la Cerda,I. Albuquerque FS, Golicher DJ (2012),"), 
-          h6("taxonstand: An r package for species names stndardisation in vegetation database." ),   
-          h6("Methods in Ecology and Evolution, 3: 1078-1083." ),   
-          h6("doi: 10.1111/j.2041-210X2012.00232.X")
-    ),
-    column(2,
-      h4("taxize package result:"),
-      tableOutput("TableL1"),
-          h5("Citation of taxize package:"),
-          h6("Scott Chamberlain and Eduard Szocs (2013). taxize -"), 
-          h6("taxonomic search and retrieval in R. F1000Research, 2:191."),    
-          h6("URL: http://f1000research.com/articles/2-191/v2.")
-          #tableOutput("Tabla33")
-      
-    )
-   
-  )
+  
+  
+           # Define UI for slider demo application
+           shinyUI(fluidPage(
+             #Application title
+            # titlePanel("Debunking Myth"),
+             #h4("Suplementary analysis for the article Bellon et al., 2016"),
+             #tags$a(href = "http://www.sagarpa.gob.mx/quienesomos/datosabiertos/siap/Paginas/Catalogos.aspx", "Base de Datos"),
+             
+             sidebarLayout(
+               sidebarPanel(
+                
+                        textInput("text", label = h3("Plant Species"), 
+                                  value = "Pinus cembroides"),
+                 
+                        h3("Actualizar"),
+                        #actionButton("update", label = "Action"),
+                        submitButton("Submit"),
+                        
+            
+                 br(),
+                 h5("comments: aponce@conabio.gob.mx"),
+                 h5("aponce73pm@gmail.com"),
+                 br(),
+                 h4("Code:"),
+                 tags$a(href = "https://github.com/APonce73/ShinyR-Taxonomic-correction", "Github"),
+                 
+                 
+                 #c("All", unique(as.character(TableL$Raza_primaria)))),
+                 
+                 
+                 
+                 width = 2),
+               fluidRow(
+                 #verbatimTextOutput("summary"),
+                 #column(9,leafletOutput("mymap", width = "1200", height = "800"))
+               
+                 column(5,
+                        h3("Taxonstand package result:"),
+                        h4(tableOutput("TableL")),
+                        h4("Citation of Taxonstand package:"),
+                        h5("Cayuela, L. Granzow-de la Cerda,I. Albuquerque FS, Golicher DJ (2012),"), 
+                        h5("taxonstand: An r package for species names stndardisation in vegetation database." ),   
+                        h5("Methods in Ecology and Evolution, 3: 1078-1083." ),   
+                        h5("doi: 10.1111/j.2041-210X2012.00232.X")
+                 ),
+                 column(3,
+                        h3("taxize package result:"),
+                        h4(tableOutput("TableL1")),
+                        h4("Citation of taxize package:"),
+                        h5("Scott Chamberlain and Eduard Szocs (2013). taxize -"), 
+                        h5("taxonomic search and retrieval in R. F1000Research, 2:191."),    
+                        h5("URL: http://f1000research.com/articles/2-191/v2.")
+                        #tableOutput("Tabla33")
+                        
+                 ),
+                 
+                 column(9,h5("All sample points (dots) come from iNaturalist database"),
+                        column(9,leafletOutput("mymap",width = "1200", height = "800")),
+                        h4("Citation of leaflet package:"),
+                        h6("Citation: : Joe Cheng and Yihui Xie (2016). Leaflet: Create Interactive web Maps with the JavaScript 'leaflet' Library"), 
+                        h6("R package version 1.0.1.900."),    
+                        h6("http://rstudio.github.io/leaflet/")
+                 )
+                 
+                 
+                 
+                 
+                 )
+               #leafletOutput("mymap", width = "100%", height = "100%")
+             )
+           )) 
+  
   
 ))
+
+
+
+##############################
+
+#shinyUI(bootstrapPage(
+#  titlePanel("Taxonomic corrections"),
+#  h6("We use two different packages in R to do almost the same analysis. The functions in R use different algorithm to correct"),
+#  h6("mispelling or synonyms of flora."),
+#  h3("Why Plant list?"),
+#  h6("Plant list is a database with the support of Missouri Botanical Garden and the Royal Botanical Gardens, Kew"),
+#  h4("Important notes:"),
+#  h6("if you decide to use this plataform, please do credits to each package in R!"),
+  
+  
+#  fluidRow(
+#    column(3, 
+#           textInput("text", label = h3("Plant Species"), 
+#                     value = "Pinus cembroides")),
+   
+#    column(3,
+#      h3("Actualizar"),
+#     #actionButton("update", label = "Action"),
+#      submitButton("Submit"),
+#      br(),
+#      br()
+#    ),
+#    column(6,
+#           h4("Taxonstand package result:"),
+#           tableOutput("TableL"),
+#           h5("Citation of Taxonstand package:"),
+#           h6("Cayuela, L. Granzow-de la Cerda,I. Albuquerque FS, Golicher DJ (2012),"), 
+#           h6("taxonstand: An r package for species names stndardisation in vegetation database." ),   
+#           h6("Methods in Ecology and Evolution, 3: 1078-1083." ),   
+#           h6("doi: 10.1111/j.2041-210X2012.00232.X")
+#    ),
+#    column(2,
+#           h4("taxize package result:"),
+#           tableOutput("TableL1"),
+#           h5("Citation of taxize package:"),
+#           h6("Scott Chamberlain and Eduard Szocs (2013). taxize -"), 
+#           h6("taxonomic search and retrieval in R. F1000Research, 2:191."),    
+#           h6("URL: http://f1000research.com/articles/2-191/v2.")
+#           #tableOutput("Tabla33")
+           
+#    ),
+#    
+#    column(10,h5("All sample points (dots) come from iNaturalist database"),
+#      column(12,leafletOutput("mymap",height =  800)),
+#      h4("Citation of leaflet package:"),
+#      h6("Citation: : Joe Cheng and Yihui Xie (2016). Leaflet: Create Interactive web Maps with the JavaScript 'leaflet' Library"), 
+#      h6("R package version 1.0.1.900."),    
+#      h6("http://rstudio.github.io/leaflet/")
+#    )
+#    
+#   
+#  )
+#  
+#))
